dc127ab7b034e23cf57c3554fb6b509e46901ec9
Plamena <yomena37@gmail.com>
2022-11-02 13:40:11 +0200

    upload recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<eda5a9fc-aa58-4146-80a8-edc8d8695826>';
-// eda5a9fc-aa58-4146-80a8-edc8d8695826
+export const KEY = '8ff17027-bcea-45ff-a7a4-9d6c875f7042';
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 import * as model from './model.js';
+import { MODAL_CLOSE_SEC } from './config.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
@@ -101,8 +102,31 @@ const controlBookmarks = function () {
 };
 
 /////////////////////////////////////////////////////////////////////////////////
-const controlAddRecipe = function (newRecipe) {
-  // Upload the new recipe data
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    // Close form window
+    setTimeout(function () {
+      // addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error('💥💥💥', err);
+    addRecipeView.renderError(err.message);
+  }
 };
 
 /////////////////////////////////////////////////////////////////////////////////
