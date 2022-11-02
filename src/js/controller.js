f6d142adda0ff2f6427369bde991b09ebb47ada3
Plamena <yomena37@gmail.com>
2022-11-02 12:38:48 +0200

    implement uploading a recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -100,6 +101,11 @@ const controlBookmarks = function () {
 };
 
 /////////////////////////////////////////////////////////////////////////////////
+const controlAddRecipe = function (newRecipe) {
+  // Upload the new recipe data
+};
+
+/////////////////////////////////////////////////////////////////////////////////
 const init = function () {
   bookmarksView.addHandlerRender(controlAddBookmark);
   recipeView.addHandlerRender(controlRecipes);
@@ -107,5 +113,6 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
